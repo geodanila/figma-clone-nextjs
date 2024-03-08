daec2e1bb302a81adac4fbb956d89950c247e74e
George Danila <geo.danila89@gmail.com>
2024-03-08 14:59:47 +0200

    added navbar with active users

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -26,13 +26,16 @@ const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: C
     return (
         <div
             className="absolute top-0 left-0"
-            style={{ transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)` }}>
+            style={{ transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)` }}
+        >
             {cursorState.mode === CursorMode.Chat && (
                 <>
                     <CursorSVG color="#000" />
 
                     <div className="absolute left-2 top-5 bg-blue-500 px-4 
-                        py-2 text-sm leading-relaxed text-white rounded-[20px]">
+                        py-2 text-sm leading-relaxed text-white rounded-[20px]"
+                        onKeyUp={(e) => e.stopPropagation()}
+                    >
                         {cursorState.previousMessage && (
                             <div>{cursorState.previousMessage}</div>
                         )}
